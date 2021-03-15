390abfb27626d81e57e87b474d7ef8ac2d2be3e9
Mateusz Małagocki <mateusz.malagocki.500@guest.p.lodz.pl>
2021-03-15 23:21:52 +0100

    Wiekszosc rzeczy pobiera sie z inputów. Metoda bisekcji jeszcze nie moze przyjac wszystkcich argumentow

--- Bisection.py
+++ Bisection.py
@@ -3,52 +3,71 @@ import matplotlib.pyplot as plt
 from tkinter import *
 import math
 from scipy.optimize import fsolve 
+from decimal import Decimal
    
 input_function = ""
 X_indexes = []
+tolerance = ""
+range_begin = ""
+range_end = ""
+
+################################# INPUT FUNCTIONS ##################################################################
+def set_input_values(function, tolerance, range_dimm):
+    set_tolerance(tolerance)
+    find_xs(function)
+    set_range(range_dimm)
+
+def set_tolerance(input_string):
+    global tolerance
+    toletance = Decimal(input_string)
     
+def set_range(input_string):
+    global range_begin
+    global range_end 
+    input_function.replace( " ", "" )
+    separator = input_string.find("," , 0)
+    range_begin = Decimal(input_string[0:separator])
+    range_end = Decimal(input_string[separator+1, len(input_string)])
+    
+
 def find_xs(input_string):
     global input_function
     global X_indexes
-    
     x_arrays = []
     first_x = input_string.find('x', 0)
     x_arrays.insert(0, first_x)
     i = 1
+    
     while ((input_string.find( 'x', x_arrays[i-1] + 1) != -1) and (input_string.find( 'x', x_arrays[i-1] + 1) != None)):
-        #print ("This is iteration of find_xs ",input_string.find( 'x', x_arrays[i-1] + 1 ))
         x_arrays.append(input_string.find( 'x', x_arrays[i-1] + 1 ))
         i += 1
         
-    #print("This is first x position ", first_x)
-    #print("This is array length ", len(x_arrays))
     X_indexes = x_arrays
     input_function = input_string
   
-
+################## MATH FUNCTIONS ###########################################################
 def f(x):
     global input_function
     global X_indexes
     
-    input_function.replace("x", str(x) )
+    input_function.replace( "x", str(x) )
     input_function.replace( " ", "" )
-    
-    #print(input_function)
     code = compile(input_function, "<string>", "eval")
     return eval(code)
 
-def bisection(a, b, tol):
-    xl = a
-    xr = b
-    
-    while (np.abs(xl-xr) >= tol):
-        c = (xl + xr)/2.0
-        prod = f(xl)*f(c)
-        if prod > tol:
-            xl = c
+def bisection():
+    global tolerance
+    global range_begin
+    global range_end    
+
+    while (np.abs(range_begin-range_end) >= tolerance):
+        c = (range_begin + range_end)/2.0
+        prod = f(range_begin)*f(c)
+        if prod > tolerance:
+            range_begin = c
         else:
-            if prod < tol:
-                xr = c
+            if prod < tolerance:
+                range_end = c
     return c
 
 ##################### WINDOW #############################################
@@ -58,15 +77,26 @@ root.geometry("200x150")
 frame = Frame(root)
 frame.pack()
  
-my_entry = Entry(frame, width = 20)
-my_entry.insert(0,'')
-my_entry.pack(padx = 5, pady = 5)
+function_input = Entry(frame, width = 20)
+function_input.insert(0,'')
+function_input.pack(padx = 5, pady = 5)
+
+tolerance_input = Entry(frame, width = 20)
+tolerance_input.insert(0,'')
+tolerance_input.pack(padx = 5, pady = 5)
+
+range_input = Entry(frame, width = 20)
+range_input.insert(0,'')
+range_input.pack(padx = 5, pady = 5)
  
-Button = Button(frame, text = "Submit", command = lambda: find_xs(my_entry.get()))
+Button = Button(frame, text = "Submit", command = lambda: set_input_values(function_input.get(), 
+                                                                           tolerance_input.get(), 
+                                                                           range_input.get()
+                                                                           ))
 Button.pack(padx = 5, pady = 5)
 root.mainloop()
 #################### MATH BELOW ##########################################
-answer = bisection(-5,5,1e-8)
+answer = bisection()
 print(" Bisection Method Gives Root At x =",answer)
 
 
