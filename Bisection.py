54284435b39757515c48cb362743395d140e4d34
Mateusz Małagocki <mateusz.malagocki.500@guest.p.lodz.pl>
2021-03-17 08:34:08 +0100

    Poprawione GUI

--- Bisection.py
+++ Bisection.py
@@ -16,18 +16,19 @@ def set_input_values(function, tolerance, range_dimm):
     set_tolerance(tolerance)
     find_xs(function)
     set_range(range_dimm)
+    math_stuff()
 
 def set_tolerance(input_string):
     global tolerance
-    toletance = Decimal(input_string)
+    tolerance = float(input_string)
     
 def set_range(input_string):
     global range_begin
     global range_end 
     input_function.replace( " ", "" )
     separator = input_string.find("," , 0)
-    range_begin = Decimal(input_string[0:separator])
-    range_end = Decimal(input_string[separator+1, len(input_string)])
+    range_begin = float(input_string[0:separator])
+    range_end = float(input_string[separator+1:])
     
 
 def find_xs(input_string):
@@ -69,42 +70,54 @@ def bisection():
             if prod < tolerance:
                 range_end = c
     return c
+def math_stuff():
+    answer = bisection()
+    print(" Bisection Method Gives Root At x =",answer)
+    
+    
+    shortcut = fsolve(f,[-1.5, 1.5])
+    print(" These are roots of function given with Fsolve ",shortcut)
+    
+    x = np.linspace(-2,2,100)
+    plt.plot(x,f(x))
+    plt.grid()
+    plt.show()    
+    
 
 ##################### WINDOW #############################################
 root = Tk()
-root.geometry("200x150")
+root.geometry("600x600")
+root.title("MM&MJ")
+
  
 frame = Frame(root)
 frame.pack()
- 
-function_input = Entry(frame, width = 20)
+
+Label(frame, text="First exercise").grid(row=0)
+
+Label(frame, text="Input:").grid(row=1)
+function_input = Entry(frame, width = 20, cursor = 'hand2')
 function_input.insert(0,'')
-function_input.pack(padx = 5, pady = 5)
+function_input.grid(row=1 , column=1, pady=10)
 
-tolerance_input = Entry(frame, width = 20)
+Label(frame, text="Input:").grid(row=2)
+tolerance_input = Entry(frame, width = 20, cursor = 'hand2')
 tolerance_input.insert(0,'')
-tolerance_input.pack(padx = 5, pady = 5)
+tolerance_input.grid(row=2 , column=1, pady=10)
 
-range_input = Entry(frame, width = 20)
+Label(frame, text="Input:").grid(row=3)
+range_input = Entry(frame, width = 20, cursor = 'hand2')
 range_input.insert(0,'')
-range_input.pack(padx = 5, pady = 5)
+range_input.grid(row=3 , column=1, pady=10)
  
 Button = Button(frame, text = "Submit", command = lambda: set_input_values(function_input.get(), 
                                                                            tolerance_input.get(), 
                                                                            range_input.get()
                                                                            ))
-Button.pack(padx = 5, pady = 5)
+Button.grid(row=4 , column=1)
+
+
 root.mainloop()
 #################### MATH BELOW ##########################################
-answer = bisection()
-print(" Bisection Method Gives Root At x =",answer)
 
-
-shortcut = fsolve(f,[-1.5, 1.5])
-print(" These are roots of function given with Fsolve ",shortcut)
-
-x = np.linspace(-2,2,100)
-plt.plot(x,f(x))
-plt.grid()
-plt.show()
 
